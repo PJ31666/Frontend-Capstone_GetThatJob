6bfa05b76e00843a748209c5053fe44ec46c70c2
Felix Palacios <flpalacios93@gmail.com>
2022-10-03 18:50:48 -0400

    add input for testing

---
+++ get-that-job/src/styles/colors.js
@@ -0,0 +1,14 @@
+const colors = {
+  black: "#1E1E1E",
+  contentSecundary: "#5C5C5C",
+  white: "#FFFFFF",
+  pink: "#F48FB1",
+  lightPink: "#FFC1E3",
+  darkPink: "#BF5F82",
+  gray: "#616161",
+  lightGray: "#8E8E8E",
+  darkgray: "#373737",
+  backgroundDark: "#8E8E8E",
+};
+
+export default colors;
