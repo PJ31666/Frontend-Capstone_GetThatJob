77d5cd50f58a441ef5e75f01c69b33c6abb98042
somuz <jsm162@live.com>
2022-10-05 17:22:12 -0500

    update landingpage and styles

--- get-that-job/src/App.js
+++ get-that-job/src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 //import CardForm from "./components/inputs/cardForm";
 //import InputForm from "./components/inputs/inputForm";
-import NavBarForm from "./components/inputs/navBar";
+import NavBarForm from "./components/nabvars/navBar";
 import HomePage from "./components/pages/HomePage";
 //import SideNavBarForm from "./components/inputs/sideNavBar";
 
--- get-that-job/src/components/pages/HomePage.js
+++ get-that-job/src/components/pages/HomePage.js
@@ -1,9 +1,18 @@
 import colors from "../../styles/colors";
 import styled from "@emotion/styled";
-import { StyledCreateAccountButton } from "../inputs/buttons";
+import { StyledCreateAccountButton } from "../buttons/buttons";
 import group from "../img/group.svg";
 import searchLogo from "../img/searchLogo.svg";
-
+import dik from "../img/dik.svg";
+import fatima from "../img/fatima.svg";
+import felix from "../img/felix.svg";
+import jorge from "../img/jorge.svg";
+import josebe from "../img/josebe.svg";
+import joset from "../img/joset.svg";
+import git from "../pic/git.svg";
+import linkedin from "../pic/linkedin.svg";
+import ruby from "../pic/ruby.svg";
+import react from "../pic/react.svg";
 export const StyledFatherContainer = styled.div`
   width: 100%;
   height: 833px;
@@ -64,7 +73,7 @@ export const StyledH1Description = styled.h1`
   line-height: 58px;
   color: ${colors.white};
 `;
-const StyledPDescription = styled.p`
+export const StyledPDescription = styled.p`
   width: 640px;
   height: 261px;
   color: ${colors.white};
@@ -72,7 +81,7 @@ const StyledPDescription = styled.p`
   font-weight: 400;
   line-height: 29px;
 `;
-const StyledImageContainer = styled.div`
+export const StyledImageContainer = styled.div`
   display: flex;
   width: 560px;
   height: 472px;
@@ -80,6 +89,98 @@ const StyledImageContainer = styled.div`
   align-items: center;
   background-color: ${colors.white};
 `;
+
+export const StyledThirdSonContainer = styled.div`
+  display: flex;
+  width: 100%;
+  height: 486px;
+  background-color: ${colors.background};
+  flex-direction: column;
+  padding: 120px;
+  gap: 26px;
+`;
+export const StyledThirdTitle = styled.h3`
+  width: 356px;
+  height: 59px;
+  margin: 0 auto;
+  color: ${colors.darkPink};
+  font-size: 48px;
+  font-weight: 400;
+  align-items: center;
+`;
+export const StyledProfileContainer = styled.div`
+  width: 1056px;
+  height: 273px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+
+  margin: 0 auto;
+`;
+export const StyledIndividualProfile = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 240px;
+  height: 273px;
+  gap: 16px;
+`;
+
+export const StyledIndividualName = styled.div`
+  width: 168px;
+  height: 29px;
+  text-align: center;
+  font-size: 24px;
+  font-weight: 400;
+`;
+export const StyledDivLinks = styled.div`
+  width: 112px;
+  height: 40px;
+  gap: 53px;
+  display: flex;
+  flex-direction: row;
+  margin: 0 auto;
+  padding-bottom: 10px;
+`;
+export const StyledLandingFooter = styled.footer`
+  display: flex;
+  width: 100%;
+  height: 73px;
+  align-items: center;
+  background-color: ${colors.background};
+`;
+export const StyledFooterContainer = styled.div`
+  width: 1200px;
+  height: 73px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 16px;
+  justify-content: space-between;
+  margin: 0 auto;
+  border-top: ${colors.darkPink} solid 1px;
+`;
+
+export const StyledCreditDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+  width: fit-content;
+  height: fit-content;
+  text-align: center;
+`;
+export const StyledResourcesDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 42px;
+  align-items: center;
+`;
+export const StyledIndividualResource = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 5px;
+  align-items: center;
+`;
+
 function HomePage() {
   return (
     <StyledFatherContainer>
@@ -116,10 +217,81 @@ function HomePage() {
           </StyledDescriptionContainer>
         </StyledDarkPinkContainer>
         <StyledImageContainer>
-          {" "}
           <img src={searchLogo} alt="Search Logo" />
         </StyledImageContainer>
       </StyledSecondSonContainer>
+      <StyledThirdSonContainer>
+        <StyledThirdTitle>Meet the team</StyledThirdTitle>
+        <StyledProfileContainer>
+          <StyledIndividualProfile>
+            <img src={dik} alt="Dik" />
+            <StyledIndividualName>Dick Solis</StyledIndividualName>
+            <StyledDivLinks>
+              <img src={git} alt="git" />
+              <img src={linkedin} alt="linkedin" />
+            </StyledDivLinks>
+          </StyledIndividualProfile>
+          <StyledIndividualProfile>
+            <img src={fatima} alt="Fatima" />
+            <StyledIndividualName>Fátima Gallardo</StyledIndividualName>
+            <StyledDivLinks>
+              <img src={git} alt="git" />
+              <img src={linkedin} alt="linkedin" />
+            </StyledDivLinks>
+          </StyledIndividualProfile>
+          <StyledIndividualProfile>
+            <img src={felix} alt="Felix " />
+            <StyledIndividualName>Felix Palacios</StyledIndividualName>
+            <StyledDivLinks>
+              <img src={git} alt="git" />
+              <img src={linkedin} alt="linkedin" />
+            </StyledDivLinks>
+          </StyledIndividualProfile>
+          <StyledIndividualProfile>
+            <img src={jorge} alt="Jorge " />
+            <StyledIndividualName>Jorge Solis</StyledIndividualName>
+            <StyledDivLinks>
+              <img src={git} alt="git" />
+              <img src={linkedin} alt="linkedin" />
+            </StyledDivLinks>
+          </StyledIndividualProfile>
+          <StyledIndividualProfile>
+            <img src={josebe} alt=" Jose Becerril" />
+            <StyledIndividualName>José Becerril</StyledIndividualName>
+            <StyledDivLinks>
+              <img src={git} alt="git" />
+              <img src={linkedin} alt="linkedin" />
+            </StyledDivLinks>
+          </StyledIndividualProfile>
+          <StyledIndividualProfile>
+            <img src={joset} alt="Jose Tordoya Logo" />
+            <StyledIndividualName>José Tordoya</StyledIndividualName>
+            <StyledDivLinks>
+              <img src={git} alt="git" />
+              <img src={linkedin} alt="linkedin" />
+            </StyledDivLinks>
+          </StyledIndividualProfile>
+        </StyledProfileContainer>
+      </StyledThirdSonContainer>
+      <StyledLandingFooter>
+        <StyledFooterContainer>
+          <StyledCreditDiv>© 202X - Get That Job</StyledCreditDiv>
+          <StyledCreditDiv>
+            Source Code:
+            <StyledResourcesDiv>
+              <StyledIndividualResource>
+                <img src={ruby} alt="Ruby" />
+                Ruby on Rails REST API
+              </StyledIndividualResource>
+              <StyledIndividualResource>
+                <img src={react} alt="React" />
+                React Responsive SPA
+              </StyledIndividualResource>
+            </StyledResourcesDiv>
+          </StyledCreditDiv>
+          <StyledCreditDiv>Codeable - Cohort 7 Final Project</StyledCreditDiv>
+        </StyledFooterContainer>
+      </StyledLandingFooter>
     </StyledFatherContainer>
   );
 }
