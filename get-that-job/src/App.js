356da55c6b23c05c8485a5fdca9e1ab9f8c60367
Felix Palacios <flpalacios93@gmail.com>
2022-10-08 17:47:06 -0400

    Add side nav bar recruiter and views

--- get-that-job/src/App.js
+++ get-that-job/src/App.js
@@ -1,24 +1,27 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
-import Home from "./components/Home/Home";
-import NavBar from "./components/NavBars/NavBar";
-import Signup1 from "./components/SignupProf/Signup1";
-import Signup2 from "./components/SignupProf/Signup2";
-import Signup3 from "./components/SignupProf/Signup3";
-import SignupRec1 from "./components/SignupRec/SignupRec1";
-import SignupRec2 from "./components/SignupRec/SignupRec2";
-import Login from "./components/Login/Login";
-import FindThatJob from "./components/ProfessionalViews/FindThatJob";
+// import { Routes, Route } from "react-router-dom";
+// import Home from "./components/Home/Home";
+// // import NavBar from "./components/NavBars/NavBar";
+// import Signup1 from "./components/SignupProf/Signup1";
+// import Signup2 from "./components/SignupProf/Signup2";
+// import Signup3 from "./components/SignupProf/Signup3";
+// import SignupRec1 from "./components/SignupRec/SignupRec1";
+// import SignupRec2 from "./components/SignupRec/SignupRec2";
+// import Login from "./components/Login/Login";
+// import FindThatJob from "./components/ProfessionalViews/FindThatJob";
 
-import MyProfile from "./components/ProfessionalViews/MyProfile";
-import Following from "./components/ProfessionalViews/Following";
-import YourApplication from "./components/ProfessionalViews/YourApplication";
+// import MyProfile from "./components/ProfessionalViews/MyProfile";
+// import Following from "./components/ProfessionalViews/Following";
+// import YourApplication from "./components/ProfessionalViews/YourApplication";
+// import JobPostings from "./components/RecruiterViews/JobPostings";
+import SideNavBarRecru from "./components/NavBars/SideNavBarRecru";
 
 function App() {
   return (
     <>
-      <NavBar />
-      <Routes>
+      {/* <NavBar /> */}
+      <SideNavBarRecru />
+      {/* <Routes>
         <Route index element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup1" element={<Signup1 />} />
@@ -30,7 +33,8 @@ function App() {
         <Route path="/YourApplication" element={<YourApplication />} />
         <Route path="/Following" element={<Following />} />
         <Route path="/MyProfile" element={<MyProfile />} />
-      </Routes>
+        <Route path="/JobPostings" element={<JobPostings />} />
+      </Routes> */}
     </>
   );
 }
